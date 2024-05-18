3a103c9cc07b555ce2fc9179482282e4c3088506
Angela Kraljevska <angela.kraljevska@yahoo.com>
2024-05-18 17:12:21 +0200

    Second branch

---
+++ index.js
@@ -0,0 +1,29 @@
+//1. 
+
+//Checking Age for Different Ticket Prices 
+//- Child ticket: $5 (below 12 years)
+//- "Teen ticket: $7" (below 18 years)
+//- "Senior ticket 
+
+//$10 (between 18 and 60)
+//- "Adult ticket: $10" (over 60years)
+
+let person = 5
+
+if (person <= 12){
+    console.log('The child ticket is $5')
+} else if (person <= 18){
+    console.log('The teen ticket is 7$')
+} else if (person >18 || person <60){
+    console.log ('The senior ticket is $10')
+}else{
+    console.log('The adult ticket is $8')
+}
+
+//2. Checking Day of the Week
+//- Monday - Start of the work week!
+//- Wednesday - Midweek!
+//- Friday - Almost weekend!
+//- Other day - Just another day.
+
+let day = 'Monday'
